4e0d12e195553ee259126c1601a692831885c0e1
Risavdeb Patra <needleweed03@gmail.com>
2025-08-11 23:24:34 +0530

    minikit provider wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@
 import { HMSRoomProvider } from "@100mslive/react-sdk";
 import "../styles/globals.css";
 import { ReactNode } from "react";
+import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { base } from "wagmi/chains";
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -17,9 +19,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <title>100ms Quickstart</title>
       </head>
       <body>
+        <MiniKitProvider
+      apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY}
+      chain={base}
+    >
         <HMSRoomProvider>
           {children}
         </HMSRoomProvider>
+        </MiniKitProvider>
       </body>
     </html>
   );
