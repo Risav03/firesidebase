e8b059b47c7746504331e9c2d379551819506482
Risavdeb Patra <needleweed03@gmail.com>
2025-08-12 00:12:25 +0530

    pfp.png added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,34 @@ import "../styles/globals.css";
 import { ReactNode } from "react";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 import { base } from "wagmi/chains";
+import { Metadata } from "next";
 
 interface RootLayoutProps {
   children: ReactNode;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL;
+  return {
+    title: "Fireside 100ms",
+    description: "This is Fireside 100ms",
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+        imageUrl: "https://100msfireside-kolt.vercel.app/pfp.png",
+        button: {
+          title: `Tune in!`,
+          action: {
+            type: "launch_frame",
+            name: "Fireside 100ms",
+            url: URL,
+            splashImageUrl: "https://100msfireside-kolt.vercel.app/pfp.png",
+            splashBackgroundColor: "#3b0404",
+          },
+        },
+      }),
+    },
+  };
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
