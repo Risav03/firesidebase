f5973df8aad397edd3f1374c88270d76438b08ee
Risavdeb Patra <needleweed03@gmail.com>
2025-11-15 13:39:18 +0530

    reposition inputs false and disable prevent scroll true

--- components/CreateRoomModal.tsx
+++ components/CreateRoomModal.tsx
@@ -139,7 +139,7 @@ export default function CreateRoomModal({ isOpen, onClose }: CreateRoomModalProp
   };
 
   return (
-    <Drawer open={isOpen} onOpenChange={onClose}>
+    <Drawer open={isOpen} onOpenChange={onClose} disablePreventScroll>
       <DrawerContent className="bg-black/95 backdrop-blur-lg text-white border-orange-500/30">
         <DrawerHeader className="border-b border-orange-500/30">
           <div className="flex items-center justify-between">
